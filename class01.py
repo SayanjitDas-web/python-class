c1bbdaa74da03e799bf3a5ca947dffbd51c2789c
SayanjitDas-web <thewebman01@gmail.com>
2026-08-06 17:44:35 +0530

    first commit

---
+++ class01.py
@@ -0,0 +1,14 @@
+# data types in python
+# int,float,str,bool,dict,list,tuple,set
+# -primitive = int,float,bool,str
+# -non_primitive = str,list,dict,tuple,set
+
+a = int(input("enter value of a - "))
+b = int(input("enter value of b - "))
+
+per = 99.99
+
+con = True 
+name = "Sayanjit"
+
+print(a+b)
