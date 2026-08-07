e36379b0e1330aa961093a2248be6df77846e7c1
SayanjitDas-web <thewebman01@gmail.com>
2026-08-07 17:29:45 +0530

    Add initial implementation of arithmetic and comparison operations

---
+++ class02.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators -> +,-,*,/,%,//,**
+# Comparision Operator -> ==,!=,>,<,>=,<=
+# Logical Operator -> and,or,not
+# Asignment Operator -> =,+=,-=,*=,/=
+# Membership Operator -> in, not in
+# Identity Operator -> is, is not
+
+# matching = True
+# print(not matching)
+
+# width = int(input("enter width: "))
+# height = int(input("enter height: "))
+
+# print("area of a rectangle is ",width*height)
+
+
+num1 = 20
+num2 = 20
+
+# if (num1 > num2):
+#     print("number1 is greater than number2")
+# else:
+#     print("number1 is not greater than number2")
+    
+    
+# print("num1 is greater than num2") if(num1 > num2) else print("num1 is not greater than num2")
+
+if(num1 > num2):
+    print("num1 is greater than num2")
+elif(num2 > num1):
+    print("num2 is greater than num1")
+else:
+    print("both are equal")
+    
+    
+print("num1 is greater than num2") if(num1 > num2) else(print("num2 is greater than num1") if(num2 > num1) else print("both are equal"))
