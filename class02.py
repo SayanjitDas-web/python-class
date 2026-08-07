b4d5aadfd6d0b04f7febda575816d598fd574f95
SayanjitDas-web <thewebman01@gmail.com>
2026-08-07 17:47:35 +0530

    Refactor conditional statements and add input handling for number classification

--- class02.py
+++ class02.py
@@ -34,3 +34,23 @@ else:
     
     
 print("num1 is greater than num2") if(num1 > num2) else(print("num2 is greater than num1") if(num2 > num1) else print("both are equal"))
+
+age = 17
+
+status = "Adult" if age >= 18 else "Minor"
+
+print(status)
+
+number = int(input("enter a number: "))
+
+# if number % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+    
+if number > 0:
+    print("positive number")
+elif number < 0:
+    print("negative number")
+else:
+    print("number is zero")
