be0cb983968fde8020a3d09d17821cef1432d5a6
SayanjitDas-web <thewebman01@gmail.com>
2026-08-14 17:19:03 +0530

    Refactor class04.py by organizing string manipulation examples and adding comments for clarity

--- class04.py
+++ class04.py
@@ -2,4 +2,14 @@
 name = "sayanjit"
 
 # print(name.count("jit"))
-print(len(name)) #lprint(len("hello"))
+# print(len(name)) #lprint(len("hello"))
+
+word = "hello" #01234
+# String indexing in Python is the process of accessing a specific, individual character within a string by using its numerical position, known as its index.
+
+# print(word.replace("e",word[1].upper()))
+
+# print(word[:3])
+# print(word[3:5])
+
+print(word[::2])
