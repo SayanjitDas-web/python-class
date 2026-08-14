7ce2ac953848f873cda3c9483d030a5622fb6edc
SayanjitDas-web <thewebman01@gmail.com>
2026-08-14 16:05:07 +0530

    Refactor class03.py by commenting out unused code and adding a simple pattern printing loop

--- class03.py
+++ class03.py
@@ -1,17 +1,28 @@
 # for loop , while loop
 
-
-target = 3
-j = 1
+# i = 1
+# target = 3
+# j = 1
 
 # while(i <= 10):
 #     print(target," X ",i," = ",i*target)
 #     i+=1
 
-while(j <= target):
-    i = 1
-    while(i <= 10):
-        print(j," X ",i," = ",i*j)
-        i+=1
-    print(" ")
-    j+=1
+# while(j <= target):
+#     i = 1
+#     while(i <= 10):
+#         print(j," X ",i," = ",i*j)
+#         i+=1
+#     print(" ")
+#     j+=1
+
+# default start value is 0 of the range function
+# for i in range(0,10,3):
+#     for j in range(2):
+#         print("in inner for loop - ",j)
+    
+# for i in [0,1,2,3,4,5,6,7,8,9]:
+#     print(i)
+
+for i in range(1,6):
+    print("*"*i)
