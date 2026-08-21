43faf1e4d6dbfff259b6487f28cdf621f9e9e2c1
SayanjitDas-web <thewebman01@gmail.com>
2026-08-21 17:38:42 +0530

    Add new file class05.py with examples of non-primitive data types

---
+++ class05.py
@@ -0,0 +1,13 @@
+#non primitive - Lists, Tuples, Sets, Dictionaries
+
+data = ["name",11,True] #0,1,2,3,4....
+
+myTuples = (1,2,3,4) # tuples
+
+mySets = { 1,2 ,3 ,4} # sets
+
+myDict = {"name":"Sayanjit","age":24} # dictionaries
+
+print(data[1])
+
+print(myDict["age"])
