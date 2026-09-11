90fd68020a395c000fd5313a2283a5c9167eb5fd
SayanjitDas-web <thewebman01@gmail.com>
2026-09-11 17:27:06 +0530

    Remove test.txt and add class09.py and class10.py with initial content and database functionality

---
+++ class10.py
@@ -0,0 +1,11 @@
+# import math
+# from math import pi, sqrt
+
+# pi = 22/7
+
+# radious = 20
+# area_of_circle = 2*pi*20*20
+
+from database import db
+
+db.create_db()
---
+++ database/db.py
@@ -0,0 +1,68 @@
+from uuid import uuid4
+
+DFL = "./database/db.txt"
+
+def create_db():
+    with open(DFL,"w") as file:
+        return "file created successfully"
+    file.close()
+    
+def add_data(data):
+    with open(DFL,"a") as file:
+        file.write(str(uuid4())+" "+data+"\n")
+        file.close()
+        
+def delete_data(id):
+    data = []
+    with open(DFL,"r") as file:
+        data = file.readlines()
+        file.close()
+        
+    target = ""
+    for line in data:
+        if id == line.split(" ")[0]:
+            target = line
+    
+    data.remove(target)
+            
+    with open(DFL,"w") as file:
+        file.write("")
+        file.close()
+        
+    with open(DFL,"a") as file:
+        for line in data:
+            file.write(line)
+        file.close()
+        
+def get_data(id):
+    target = ""
+    with open(DFL,"r") as file:
+        for line in file.readlines():
+            if id == line.split(" ")[0]:
+                target = line
+        file.close()
+        
+    return target
+
+def update_data(id,newData):
+    data = []
+    with open(DFL,"r") as file:
+        data = file.readlines()
+        file.close()
+        
+    target = ""
+    for line in data:
+        if id == line.split(" ")[0]:
+            target = line
+            
+    target_index = data.index(target)
+    data[target_index] = id+" "+newData+"\n"
+    
+    with open(DFL,"w") as file:
+        file.write("")
+        file.close()
+        
+    with open(DFL,"a") as file:
+        for line in data:
+            file.write(line)
+        file.close()
