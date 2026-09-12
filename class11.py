ce6db59ccad3118a5d1d500fa7e1089beb4d6ade
SayanjitDas-web <thewebman01@gmail.com>
2026-09-12 17:28:27 +0530

    Enhance class11.py with data management options and update db.txt by removing unused data entry

--- class11.py
+++ class11.py
@@ -1,5 +1,5 @@
 import math
-from database.db import add_data, get_all_data
+from database.db import add_data, get_all_data, delete_data, update_data
 
 def render_table():
     for i in range(len(get_all_data()[0])+2):
@@ -18,4 +18,30 @@ def render_table():
     for data in get_all_data():
         print(data)
         
-render_table()
+while True:
+    print("""
+          enter 'a' for adding data
+          enter 'd' for deleteing a data
+          enter 's' for see all data
+          enter 'u' for update a data
+          enter 'e' for exit
+    """)
+    op = input("enter a option -> ")
+    print()
+    if op == "a":
+        data = input("enter your data -> ")
+        add_data(data)
+    elif op == "d":
+        id = input("enter the id -> ")
+        delete_data(id)
+    elif op == "u":
+        id = input("enter the id -> ")
+        newData = input("enter the new data -> ")
+        update_data(id,newData)
+    elif op == "s":
+        for data in get_all_data():
+            print(data)
+    elif op == "e":
+        break
+    else:
+        print("please select an option!")
